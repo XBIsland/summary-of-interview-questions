0e604f55fc0cfa879b17af2986dae373942991a0
XBIsland <XBIsland@users.noreply.github.com>
2022-09-17 09:25:26 +0000

    deploy: c8ead353fe353bf3334a83512be052d61ebdbbbf

---
+++ assets/base_two.md.962a39ef.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.8d5e1b67.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"base/two.md"}'),o={name:"base/two.md"};function r(s,c,n,p,_,d){return a(),t("div")}const f=e(o,[["render",r]]);export{m as __pageData,f as default};
